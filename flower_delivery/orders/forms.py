419f362b8faf614cdf29216e1874a3b7bb65d59a
Dmitry Frolov <72983017+a08037@users.noreply.github.com>
2025-02-28 11:52:14 +0200

    Update forms.py

--- flower_delivery/orders/forms.py
+++ flower_delivery/orders/forms.py
@@ -1,9 +1,11 @@
-from .models import Order, Flower
+from .models import Order, Flower, Review, Rating, Report
 from django import forms
+
+# Форма для оформления заказа
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ['quantity', 'flower', 'address', 'delivery_time', 'delivery_date']  # Указываем названия полей из models.py
+        fields = ['quantity', 'flower', 'address', 'delivery_time', 'delivery_date']
         labels = {
             'quantity': 'Количество',
             'flower': 'Букет',
@@ -12,8 +14,26 @@ class OrderForm(forms.ModelForm):
             'delivery_date': 'Дата доставки'
         }
 
-        flower = forms.ModelChoiceField(queryset=Flower.objects.all(), empty_label="Выберите букет")
+    flower = forms.ModelChoiceField(queryset=Flower.objects.all(), empty_label="Выберите букет", required=False)
+    quantity = forms.IntegerField(min_value=1, required=False)
+    address = forms.CharField(max_length=255, required=True)
+    delivery_date = forms.DateField(widget=forms.SelectDateWidget(), required=True)
+    delivery_time = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}), required=True)
 
-class ReviewForm(forms.Form):
-    rating = forms.ChoiceField(choices=[(i, i) for i in range(1, 6)], widget=forms.RadioSelect, label="Рейтинг")
-    comment = forms.CharField(widget=forms.Textarea, label="Комментарий", required=False)
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['comment']
+class RatingForm(forms.ModelForm):
+    class Meta:
+        model = Rating
+        fields = ['rating']  # Поле для выставления рейтинга (например, от 1 до 5)
+
+class ReportForm(forms.ModelForm):
+    class Meta:
+        model = Report
+        fields = ['start_date', 'end_date']
+        labels = {
+            'start_date': 'Дата начала периода',
+            'end_date': 'Дата окончания периода',
+        }
